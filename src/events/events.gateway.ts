73a4ff0bce260c4cec2b8555c87fb46d4cd6d72b
Mohamed_aboelnasr <moohamedaboelnasr@outlook.com>
2025-07-20 19:12:06 +0300

    subscribe to hi-server and hello events

--- src/events/events.gateway.ts
+++ src/events/events.gateway.ts
@@ -1,6 +1,12 @@
 import { OnModuleInit } from '@nestjs/common';
-import { WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
-import { Server } from 'socket.io';
+import {
+  ConnectedSocket,
+  MessageBody,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
 
 @WebSocketGateway()
 export class EventsGateway implements OnModuleInit {
@@ -15,5 +21,25 @@ export class EventsGateway implements OnModuleInit {
         console.log(`🔴Client disconnected with id: ${socket.id}`);
       });
     });
+  }
+
+  @SubscribeMessage('hi-server')
+  handelHiEvent(@MessageBody() body: any) {
+    console.log('Received hi-server event with body:', body);
+    this.server.emit('hi-client', { message: 'Hi from server!' });
+  }
+
+  @SubscribeMessage('hello')
+  handleHelloEvent(
+    @ConnectedSocket() client: Socket,
+    @MessageBody() data: string,
+  ) {
+    console.log(
+      'Received hello event with body:',
+      data,
+      'from client:',
+      client.id,
+    );
+    client.emit('hello-client', { message: 'Hello from server!' });
   }
 }
