ca66db1aa0947c7a392d19d6729d321f299bc90b
Mohamed_aboelnasr <moohamedaboelnasr@outlook.com>
2025-07-23 16:14:32 +0300

    chore(images): configure ImagesModule

--- src/images/images.module.ts
+++ src/images/images.module.ts
@@ -1,4 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ImagesController } from './images.controller';
+import { ImagesService } from './images.service';
+import { UploadModule } from 'src/upload/upload.module';
+import { EventsModule } from 'src/events/events.module';
+import { AuthModule } from 'src/auth/auth.module';
+import { JwtModule } from '@nestjs/jwt';
 
-@Module({})
+@Module({
+  imports: [UploadModule, EventsModule, AuthModule, JwtModule],
+  controllers: [ImagesController],
+  providers: [ImagesService],
+  exports: [ImagesService],
+})
 export class ImagesModule {}
