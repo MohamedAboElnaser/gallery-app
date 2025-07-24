a7bf9f49e8a11467544aaa1c7a4cb74ee57a3a48
Mohamed_aboelnasr <moohamedaboelnasr@outlook.com>
2025-07-24 22:04:50 +0300

    feat(images): add `DELETE /images/bulk-delete` endpoint

--- src/images/images.controller.ts
+++ src/images/images.controller.ts
@@ -10,6 +10,7 @@ import {
   UsePipes,
   Get,
   Query,
+  Delete,
 } from '@nestjs/common';
 import { FilesInterceptor } from '@nestjs/platform-express';
 import { ImageValidationPipe } from 'src/common/pipes';
@@ -17,6 +18,7 @@ import { ImagesService } from './images.service';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { UploadImagesDto } from './dto/upload-images.dto';
 import { GetImagesDto } from './dto/get-images.dto';
+import { BulkDeleteImagesDto } from './dto/bulk-delete.dto';
 
 @UsePipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }))
 @Controller('images')
@@ -47,5 +49,15 @@ export class ImagesController {
   @Get()
   async getImages(@Query() query: GetImagesDto, @Request() req: any) {
     return await this.imagesService.getImages(req.user.sub, query);
+  }
+
+  @Delete('bulk-delete')
+  async bulkDeleteImages(@Body() body: BulkDeleteImagesDto) {
+    const res = await this.imagesService.deleteImages(body.imageIds);
+    return {
+      message: 'Images deleted successfully',
+      deletedCount: res.length,
+      failedCount: body.imageIds.length - res.length,
+    };
   }
 }
